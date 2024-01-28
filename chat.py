5113beb6763979f0104da5db8e6691f2076efa59
elmerurbina <[elmerurbina570@gmail.com]>
2024-01-27 20:48:05 -0600

    update

--- chat.py
+++ chat.py
@@ -4,6 +4,10 @@ from flask import Flask, render_template
 app = Flask(__name__, static_url_path='/static')
 
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 
 @app.route('/chat')
 def chat():
--- static/logout.js
+++ static/logout.js
@@ -1,12 +1,11 @@
-// script.js
-
+// Assuming you have a button with the id "logoutButton"
 document.getElementById('logoutButton').addEventListener('click', function() {
     // Show a confirmation dialog
     var confirmation = confirm('¿Seguro que desea salir?');
 
-    // If the user clicks "OK" (true), close the window/tab
+    // If the user clicks "OK" (true), redirect to index.html
     if (confirmation) {
-        window.close();
+        window.location.href = '/';
     }
     // If the user clicks "Cancel" (false), do nothing
 });
