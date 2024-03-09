7a7d449161f9e9b61f7740b4811358059a4e3880
elmerurbina <[elmerurbina570@gmail.com]>
2024-03-09 16:42:35 -0600

    Update files

--- static/js/GoogleSignIn.js
+++ static/js/GoogleSignIn.js
@@ -2,8 +2,18 @@
 
 function onSignIn(googleUser) {
     var id_token = googleUser.getAuthResponse().id_token;
+     // Send the id_token to your backend for authentication
+     fetch('/google_login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+            'id_token': id_token
+        })
+    })
 
-   then(function(response) {
+   .then(function(response) {
         // Handle the response from the backend
         if (response.ok) {
             window.location.href = "{{ url_for('chat') }}";  // Redirect to chat interface using url_for
