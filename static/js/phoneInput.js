198635da14df94dcc15dfe73f094c741cd685aa7
elmerurbina <[elmerurbina570@gmail.com]>
2024-04-05 14:52:48 -0600

    Update files

---
+++ static/js/phoneInput.js
@@ -0,0 +1,20 @@
+var input1 = document.querySelector("#contact-one");
+var input2 = document.querySelector("#contact-two");
+
+var iti1 = window.intlTelInput(input1, {
+    separateDialCode: true,
+    initialCountry: "ni",
+    geoIpLookup: function(success, failure) {
+        // Make an AJAX request to get the user's country code
+        // and call success(countryCode) or failure() accordingly
+    },
+});
+
+var iti2 = window.intlTelInput(input2, {
+    separateDialCode: true,
+    initialCountry: "ni",
+    geoIpLookup: function(success, failure) {
+        // Make an AJAX request to get the user's country code
+        // and call success(countryCode) or failure() accordingly
+    },
+});
