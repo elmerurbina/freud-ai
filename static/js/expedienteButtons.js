58a38258eb3c9febbfc0e67dc7762edd1c9aaf3a
elmerurbina <[elmerurbina570@gmail.com]>
2024-05-24 12:15:40 -0600

    Updates

--- static/js/expedienteButtons.js
+++ static/js/expedienteButtons.js
@@ -1,66 +1,52 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Get the modal and the passkey input field
-    var modal = document.getElementById('passkeyModal');
-    var passkeyInput = document.getElementById('passkeyInput');
-    var passkeySubmit = document.getElementById('passkeySubmit');
-    var errorMessage = document.getElementById('error-message');
+document.addEventListener('DOMContentLoaded', () => {
+    const addButtons = document.querySelectorAll('.add-button');
+    const passkeyModal = document.getElementById('passkeyModal');
+    const passkeyInput = document.getElementById('passkeyInput');
+    const passkeySubmit = document.getElementById('passkeySubmit');
+    const errorMessage = document.getElementById('error-message');
+    let currentFormType = '';
 
-    // Get all the add buttons
-    var addButtons = document.querySelectorAll('.add-button');
-
-    // Attach event listener to each add button
-    addButtons.forEach(function(button) {
-        button.addEventListener('click', function() {
-            // Show the passkey modal
-            modal.dataset.form = button.dataset.form;
-            modal.style.display = 'block';
+    addButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            currentFormType = button.getAttribute('data-form');
+            passkeyModal.style.display = 'block';
         });
     });
 
-    // When the user clicks on the close button, close the modal
-    var closeButton = document.querySelector('.close-button');
-    closeButton.addEventListener('click', function() {
-        modal.style.display = 'none';
-        errorMessage.textContent = '';
-    });
+    passkeySubmit.addEventListener('click', () => {
+        const passkey = passkeyInput.value;
+        const formData = new FormData();
+        formData.append('passkey', passkey);
+        formData.append('form_type', currentFormType);
 
-    // When the user clicks outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = 'none';
-            errorMessage.textContent = '';
-        }
-    };
-
-    // Add event listener for passkey submission
-    passkeySubmit.addEventListener('click', function() {
-        var passkey = passkeyInput.value;
-        var formType = modal.dataset.form;
-        // Make a request to the server to validate the passkey
         fetch('/validate_passkey', {
             method: 'POST',
-            body: JSON.stringify({ passkey: passkey, form_type: formType }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
+            body: formData
         })
-        
-        .then(response => {
-            if (response.status === 200) { // Check if response status is OK
-                // Passkey is correct, display the form associated with the button
-                var form = document.getElementById(formType);
-                modal.style.display = 'none';
-                form.style.display = 'block';
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                document.getElementById(currentFormType).style.display = 'block';
+                passkeyModal.style.display = 'none';
+                passkeyInput.value = '';
+                errorMessage.textContent = '';
             } else {
-                // Passkey is incorrect, display an error message
-                errorMessage.textContent = 'Clave de acceso incorrecta. Inténtalo de nuevo.';
+                errorMessage.textContent = 'Clave incorrecta, por favor intentalo nuevamente!';
                 errorMessage.style.color = 'red';
                 errorMessage.style.fontWeight = 'bold';
             }
         })
-        
         .catch(error => {
             console.error('Error:', error);
+            errorMessage.textContent = 'Ocurrio un error, por favor intentalo de nuevo!.';
+            errorMessage.style.color = 'red';
+            errorMessage.style.fontWeight = 'bold';
         });
+    });
+
+    document.querySelector('.close-button').addEventListener('click', () => {
+        passkeyModal.style.display = 'none';
+        passkeyInput.value = '';
+        errorMessage.textContent = '';
     });
 });
